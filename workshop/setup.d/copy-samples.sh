692bf71145b8156a919c19faba5c65eadf473d5c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-26 17:04:14 +1100

    Add debug logging.

--- workshop/setup.d/copy-samples.sh
+++ workshop/setup.d/copy-samples.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+set -x
+
+ls -las
+
 if [ ! -d /home/eduk8s/notebook/workspace/samples ]; then
+    ls -las /home/eduk8s/notebook/workspace
     cp -rp /home/eduk8s/samples /home/eduk8s/notebook/workspace/
+    ls -las /home/eduk8s/notebook/workspace
     fix-permissions /home/eduk8s/notebook/workspace/samples
 fi
