a09dc94cf198e8623f3dedae7f9681008214a317
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-26 17:09:41 +1100

    Change mount structure.

--- workshop/setup.d/copy-samples.sh
+++ workshop/setup.d/copy-samples.sh
@@ -2,11 +2,7 @@
 
 set -x
 
-ls -las
-
-if [ ! -d /home/eduk8s/notebook/workspace/samples ]; then
-    ls -las /home/eduk8s/notebook/workspace
-    cp -rp /home/eduk8s/samples /home/eduk8s/notebook/workspace/
-    ls -las /home/eduk8s/notebook/workspace
-    fix-permissions /home/eduk8s/notebook/workspace/samples
+if [ ! -d /home/eduk8s/workspace/samples ]; then
+    cp -rp samples workspace/samples
+    fix-permissions workspace/samples
 fi
